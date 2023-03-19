91e03cf54308fc145e22798c1ea3941d5c46904f
Sanyam <sanyamgoyal2859@gmail.com>
2023-03-20 01:44:40 +0530

    make inline function for group color

--- src/App.js
+++ src/App.js
@@ -14,20 +14,26 @@ const App = () => {
     setGroups(storedGroups);
     const storedNotes = JSON.parse(localStorage.getItem("notes")) || [];
     setNotes(storedNotes);
-  },[]);
+  }, []);
 
   const addGroup = (groupName, color) => {
-     const group = {groupName, color};
-     if(groupName === "")return;
+    const group = { groupName, color };
+    if (groupName === "") return;
     const updatedGroups = [...groups, group];
     setGroups(updatedGroups);
     localStorage.setItem("groups", JSON.stringify(updatedGroups));
   };
-  
+
   const addNote = (text) => {
     const id = Date.now().toString();
     const group = window.location.pathname.split("/")[2];
-    const note = { id, text, group, date: new Date().toString(), lastUpdated: new Date()};
+    const note = {
+      id,
+      text,
+      group,
+      date: new Date().toString(),
+      lastUpdated: new Date(),
+    };
     const updatedNotes = [...notes, note];
     setNotes(updatedNotes);
     localStorage.setItem("notes", JSON.stringify(updatedNotes));
@@ -37,31 +43,37 @@ const App = () => {
     return notes.filter((note) => note.group === group);
   };
 
-  const groupColor = (groupName) => {
-    let name = groupName.replace("%20", " ");
-    for(let i=0; i<groups.length; i++){
-      if(groups[i].groupName === name){
-        return groups[i].color;
-      }
-    }
-  }
+  const groupColor = (groupName) => {};
   return (
     <Router>
       <div className="app-container">
         <div className="group-list-container">
           <div className="group-title">Pocket Notes</div>
           <AddGroupDialog addGroup={addGroup} />
-          <GroupList groups={groups} clickCount = {clickCount} setClickCount = {setClickCount} />
+          <GroupList
+            groups={groups}
+            clickCount={clickCount}
+            setClickCount={setClickCount}
+          />
         </div>
         <div className="group-container">
           <Routes>
             <Route path="/" element={<p>Welcome to Notes App</p>} />
             <Route
-              exact path="/group/:group"
+              exact
+              path="/group/:group"
               element={
                 <Group
                   group={window.location.pathname.split("/")[2]}
-                  color={groupColor("")}
+                  color={() => {
+                    let groupName = window.location.pathname.split("/")[2];
+                    let name = groupName.replace("%20", " ");
+                    for (let i = 0; i < groups.length; i++) {
+                      if (groups[i].groupName === name) {
+                        return groups[i].color;
+                      }
+                    }
+                  }}
                   notes={getNotesByGroup(
                     window.location.pathname.split("/")[2]
                   )}
